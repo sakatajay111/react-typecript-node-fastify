3f399e41b624fd6ac07eb8dfd7a3c25b715a44d6
Ajay Sakat <sakatajay111@gmail.com>
2021-07-21 00:41:14 +0530

    initial commit

---
+++ containers/backend/src/index.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/ping', async (request, reply) => {
+  return 'pong b \n'
+})
+
+server.listen(3001, '0.0.0.0', function (err, address) {
+  	if (err) {
+		server.log.error(err)
+		process.exit(1)
+	}
+	server.log.info(`server listening on ${address}`)
+});
---
+++ containers/frontend/src/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var react_dom_1 = require("react-dom");
+require("./index.scss");
+react_dom_1["default"].render(react_1["default"].createElement("h1", null, "Hello React!"), document.getElementById('app-root'));
